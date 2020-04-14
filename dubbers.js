756f2fe54d99bb020c5780baa3827e35a262df90
Daaren Urthling <daaren.urthling@hotmail.com>
2020-04-15 00:01:30 +0200

    primitiva funzione di ricerca

--- dubbers.js
+++ dubbers.js
@@ -1,5 +1,6 @@
 var dubbersList = {
-  nextPage : function() {}
+  nextPage : function() {},
+  search : function() {}
 };
 
 ( dubbersList => {
@@ -8,26 +9,51 @@ var dubbersList = {
   const dubbersListUI = document.getElementById("dubbersList");
   dubbersListUI.innerHTML = "";
 
-  function showPage(dubbersRef) {
-      dubbersRef.on("value", snap => {
-      snap.forEach(dubSnap => {
-        var dubber = dubSnap.val();
-        if (dubber.name == lastShown)
-          return;
-        $("#dubbersList").append(
-          utils.render($('#dubber-template').html(), {
-            name: dubber.name, 
-            photo: dubber.photo ? utils.remoteURL(dubber.photo.name) : "images/no-dubber-photo.jpg",
-            alt: dubber.photo ? dubber.photo.description : "nessuna immagine"
-        }));
-        lastShown = dubber.name;
-      });
-    });
-  }
+  // function showPage(dubbersRef) {
+  //     dubbersRef.on("value", snap => {
+  //     snap.forEach(dubSnap => {
+  //       var dubber = dubSnap.val();
+  //       if (dubber.name == lastShown)
+  //         return;
+  //       $("#dubbersList").append(
+  //         utils.render($('#dubber-template').html(), {
+  //           name: dubber.name, 
+  //           photo: dubber.photo ? utils.remoteURL(dubber.photo.name) : "images/no-dubber-photo.jpg",
+  //           alt: dubber.photo ? dubber.photo.description : "nessuna immagine"
+  //       }));
+  //       lastShown = dubber.name;
+  //     });
+  //   });
+  // }
+
+  function showPage(dubbersRef, filter) {
+    dubbersRef.on("child_added", snap => {
+      var dubber = snap.val();
+      if (filter && !dubber.name.toUpperCase().includes(filter.toUpperCase()))
+        return;
+      if (dubber.name == lastShown)
+        return;
+      $("#dubbersList").append(
+        utils.render($('#dubber-template').html(), {
+          name: dubber.name, 
+          photo: dubber.photo ? utils.remoteURL(dubber.photo.name) : "images/no-dubber-photo.jpg",
+          alt: dubber.photo ? dubber.photo.description : "nessuna immagine"
+      }));
+      lastShown = dubber.name;
+  });
+}
 
   dubbersList.nextPage = function() {
     const dubbersRef = database.ref().child('dubbers').orderByChild('name').startAt(lastShown).limitToFirst(11);
     showPage(dubbersRef);
+  }
+
+  dubbersList.search = function() {
+    var list = $("#dubbersList");
+    $("#dubbersList")[0].innerHTML = "";
+    var filter = $("#filter").val();
+    const dubbersRef = database.ref().child('dubbers');
+    showPage(dubbersRef, filter);
   }
 
   var database = firebase.database();
